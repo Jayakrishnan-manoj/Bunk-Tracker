9b47e690f5fd64077d039a2b7c22f46748985901
Jayakrishnan-manoj <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2023-01-16 20:24:45 +0530

    added feature to update days

--- lib/widgets/subject_item.dart
+++ lib/widgets/subject_item.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SubjectItem extends StatelessWidget {
+class SubjectItem extends StatefulWidget {
   final String title;
   final String id;
 
   SubjectItem(this.title, this.id);
+
+  @override
+  State<SubjectItem> createState() => _SubjectItemState();
+}
+
+class _SubjectItemState extends State<SubjectItem> {
+  int attendance = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +27,59 @@ class SubjectItem extends StatelessWidget {
         child: Column(
           children: [
             Text(
-              title,
+              widget.title,
               style: const TextStyle(
                 color: Colors.white,
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      elevation: 7,
+                      backgroundColor:
+                          Theme.of(context).appBarTheme.backgroundColor,
+                      shape: CircleBorder()),
+                  onPressed: () {
+                    setState(() {
+                      attendance--;
+                    });
+                  },
+                  child: const Icon(
+                    FontAwesomeIcons.minus,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  attendance.toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      elevation: 7,
+                      backgroundColor:
+                          Theme.of(context).appBarTheme.backgroundColor,
+                      shape: CircleBorder()),
+                  onPressed: () {
+                    setState(() {
+                      attendance++;
+                    });
+                  },
+                  child: const Icon(
+                    FontAwesomeIcons.plus,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
           ],
         ),

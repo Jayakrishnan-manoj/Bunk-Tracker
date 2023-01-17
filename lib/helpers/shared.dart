bd4177d7febee803a33f7211de40bade08197125
Jayakrishnan-manoj <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2023-01-17 23:59:15 +0530

    add shared preferences

---
+++ lib/helpers/shared.dart
@@ -0,0 +1,6 @@
+// import 'package:shared_preferences/shared_preferences.dart' ;
+
+// Future<void> saveAttendance(int val)async {
+//   final sf= await SharedPreferences.getInstance();
+//   sf.setInt(, val);
+// }

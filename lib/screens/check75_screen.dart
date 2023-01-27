fe701e477f0b86211232224d3e11bc0b064f50a7
Jayakrishnan-manoj <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2023-01-27 23:58:31 +0530

    add: attendance form

--- lib/screens/check75_screen.dart
+++ lib/screens/check75_screen.dart
@@ -10,43 +10,61 @@ class AttendanceCheck extends StatefulWidget {
 }
 
 class _AttendanceCheckState extends State<AttendanceCheck> {
+  TextEditingController _presentController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: kTextColor,
         title: const Text("Attendance75"),
       ),
       drawer: AppDrawer(
         isSelected: false,
       ),
-      // body: Padding(
-      //   padding: const EdgeInsets.only(
-      //     left: 20,
-      //     right: 20,
-      //     bottom: 50,
-      //     top: 10,
-      //   ),
-      //   child: Column(
-      //     children: [
-      //       Row(
-      //         children: [
-      //           Container(
-      //             child: const Text(
-      //               "Present",
-      //               style: TextStyle(
-      //                   color: Colors.white, fontWeight: FontWeight.w700),
-      //             ),
-      //           ),
-      //           TextFormField(
-      //             decoration: textInputDecoration.copyWith(
-      //               labelText: "Present"
-      //             ),
-      //           ),
-      //         ],
-      //       )
-      //     ],
-      //   ),
-      // ),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Card(
+                color: kTextColor,
+                elevation: 5,
+                child: Form(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _presentController,
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2.0),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2.0),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            hintText: 'Present',
+                            labelText: 'No. of days present',
+                            labelStyle: TextStyle(
+                              color: kAppBarColor,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
